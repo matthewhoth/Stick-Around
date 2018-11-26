33b2dcabfdb6bedcec5328d93256a404244cc599
Matthew Hoth <matthewhoth@gmail.com>
2018-11-26 20:44:09 +0800

    Import the Meta and Header components into the Page class

--- client/components/Page.js
+++ client/components/Page.js
@@ -1,9 +1,12 @@
 import React from "react";
-
+import Header from "./Header";
+import Meta from "./Meta";
 export default class Page extends React.PureComponent {
   render() {
     return (
       <div>
+        <Meta />
+        <Header />
         <p>Catch me up top how about that</p>
         {this.props.children}
       </div>
